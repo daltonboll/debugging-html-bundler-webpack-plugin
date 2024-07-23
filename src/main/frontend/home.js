635705bbd06a213b8606087396dc8510da29ea08
Dalton Boll <daltonboll@gmail.com>
2024-07-23 06:50:25 -0700

    Switched .css to .scss

--- src/main/frontend/home.js
+++ src/main/frontend/home.js
@@ -1,4 +1,4 @@
-// import "./styles.css?inline"; // <= If I comment this out, the application runs without error and the JS is executed on the page just fine (the CSS is just absent from the webpage)
-// import "./styles.css"; // <= this syntax also causes an error
+// import "./styles.scss?inline"; // <= If I comment this out, the application runs without error and the JS is executed on the page just fine (the CSS is just absent from the webpage)
+// import "./styles.scss"; // <= this syntax also causes an error
 
 console.log('hello world!');
